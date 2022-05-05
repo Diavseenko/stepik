c4ad305afb1680907d554a81bb2e45ac7e588f04
DmitrY <diavseenko@yandex.ru>
2022-05-05 15:53:11 +0300

    Actions with alert window

---
+++ alert.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+import time
+import math
+def calc(x):
+   return str(math.log(abs(12*math.sin(int(x)))))
+
+link = "http://suninjuly.github.io/alert_accept.html"
+
+try:
+   browser=webdriver.Chrome()
+   browser.get(link)
+
+   button = browser.find_element_by_class_name("btn")
+   button.click()
+
+   modal = browser.switch_to.alert
+   modal.accept()
+
+   elemX = browser.find_element_by_id("input_value").text
+   
+
+   key = calc(int(elemX))
+
+   inputField = browser.find_element_by_id("answer")
+   inputField.send_keys(key)
+
+   sendButt = browser.find_element_by_class_name("btn")
+
+   
+   sendButt.click()
+
+
+finally:
+   time.sleep(20)
+   browser.quit()   
+
+
